f5a22e51ad1458a831eba02e34f44083266f5f64
kkaska <kkaskasoli@gmail.com>
2019-09-19 22:31:05 +0100

    Installed bootstrap into project

--- resources/views/Layouts/app.blade.php
+++ resources/views/Layouts/app.blade.php
@@ -1,13 +1,8 @@
 <!DOCTYPE html>
 <html>
   <head>
-      <meta charset="utf-8">
-      <title>Craneware: Medical Procedure Locator</title>
-  </head>
 
-  <h1>Craneware: Medical Procedure Locator</h1>
+      @yield('content')
 
-  <body>
-    @yield('content')
   </body>
 </html>
