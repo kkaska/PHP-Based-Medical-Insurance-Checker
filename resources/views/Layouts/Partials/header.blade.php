0c041589ada3c7618e86dbcf5b4c3dff087a9317
kkaska <kkaskasoli@gmail.com>
2019-09-20 13:46:12 +0100

    fix

--- resources/views/Layouts/Partials/header.blade.php
+++ resources/views/Layouts/Partials/header.blade.php
@@ -1,10 +1,13 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
-    <a href="home" class="navbar-brand"><img src="https://img.icons8.com/dotty/50/000000/find-hospital.png">HealthScanner</a>
+    <a href="{{ URL::to('/') }}/home" class="navbar-brand">Craneware: Medical Procedure Locator</a>
 
     <div class="collapse navbar-collapse">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item">
-                <a class="nav-link" href="hospitals">Hospitals</a>
+                <a class="nav-link" href="{{ URL::to('/') }}/search">Search</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ URL::to('/') }}/hospitals">Hospitals</a>
             </li>
     </div>
 </nav>
