c18325de4a2c1ffd0603af450923451ba7bf75f7
kkaska <kkaskasoli@gmail.com>
2019-10-04 11:02:28 +0100

    removed old links that are not used

--- resources/views/Layouts/Partials/header.blade.php
+++ resources/views/Layouts/Partials/header.blade.php
@@ -7,12 +7,6 @@
                         <li class="nav-item">
                             <a class="nav-link font-weight-bold text-darkgray" href="{{ URL::to('/') }}/home">Home</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link font-weight-bold text-darkgray" href="{{ URL::to('/') }}/search">Search</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link font-weight-bold text-darkgray" href="{{ URL::to('/') }}/hospitals">Hospitals</a>
-                        </li>
                     </ul>
                 </nav>
             </div>
