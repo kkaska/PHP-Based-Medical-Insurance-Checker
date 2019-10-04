46517252f8a87da16bd447d7263b3bf5949c6c3f
kkaska <kkaskasoli@gmail.com>
2019-10-04 13:07:32 +0100

    changed city to location

--- resources/views/Layouts/Partials/search.blade.php
+++ resources/views/Layouts/Partials/search.blade.php
@@ -28,8 +28,8 @@
                         <div class="col-xl-4">
                             <div class="form-group">
                                 <div class="input-group">
-                                    <input type="text" class="form-control form-control-lg" id="city" name="city" aria-label="City" placeholder="City" required>
-                                    <label style="display: none" for="city">Address</label>
+                                    <input type="text" class="form-control form-control-lg" id="city" name="city" aria-label="City" placeholder="Location" required>
+                                    <label style="display: none" for="city">city</label>
                                     <div class="input-group-append">
                                         <button id="Click to find location" class="btn btn-success text-darkgray font-weight-bold" type="button">Find</button>
                                     </div>
